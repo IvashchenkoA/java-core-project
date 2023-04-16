e49e899484ee08a007b7002785e6c26d5f8f9dc5
imbashchenko <nastya1904iva@gmail.com>
2023-04-16 14:24:36 +0200

    added thread which writes to file

---
+++ WriteToFile.java
@@ -0,0 +1,54 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class WriteToFile extends Thread{
+    public Trainset[] trainsArray;
+    public String file = "AppState.txt";
+
+    public WriteToFile(Trainset[] arr){
+        this.trainsArray = arr;
+    }
+    public void run(){
+        while(!Thread.interrupted()) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                    for (Trainset trainset : trainsArray) {
+                        while (trainset.locomotive.route == null){
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                    sortTrainsets();
+                    writer.write("List of trainsets:\n");
+                    for(Trainset trainset : trainsArray)
+                        writer.write(trainset.toString());
+                }
+                catch(Exception e){
+                    throw new RuntimeException(e);
+                }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    public void sortTrainsets(){
+        for(int i = 0; i < trainsArray.length - 1; i++){
+            int maxIndex = i;
+            for(int j = i + 1; j < trainsArray.length; j++){
+                if(trainsArray[j].locomotive.calcDistance() > trainsArray[maxIndex].locomotive.calcDistance()){
+                    maxIndex = j;
+                }
+            }
+            Trainset tmp = trainsArray[maxIndex];
+            trainsArray[maxIndex] = trainsArray[i];
+            trainsArray[i] = tmp;
+        }
+    }
+}
